a04c3dc353a9bd330269227e4d2653014a523d16
Rain Erich Ehasalu <Rain.Erich.Ehasalu@vikk.ee>
2026-04-07 18:30:17 +0300

    Teine commit

--- tervitus.sh
+++ tervitus.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
-kp=`date`
-echo "tere! Tänane kuupäev on: $kp"
+#Skript tevitab kasutajat ja kuvab kuupäeva
+
+#kuuvab tervituse
+echo "Tere tulemast Bash skriptimise kursusele"
+echo ""
+#kuuvab teate ja tänase kuupäeva
+echo "Täna on:"
+date
+echo ""
+#kuvab lõpusõnumi
+echo "See on hea päev Bäsh skriptimise alustamiseks"
